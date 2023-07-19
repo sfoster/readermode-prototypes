2380c1fb992d13d3a696e4f78faf32015e15c776
Sam Foster <sam@sam-i-am.com>
2023-07-18 23:11:38 -0700

    Wire up the letterbox highlighter and make a way to choose which one to use

--- highlight-box.js
+++ highlight-box.js
@@ -5,86 +5,79 @@ import {
 } from "./lit.all.mjs.js";
 import { MozLitElement } from "./lit-utils.mjs.js";
 
-class HighlightBox extends MozLitElement {
+export class HighlightBox extends MozLitElement {
   constructor() {
     super();
-    this.rect = {
+    this.lineRect = {
+      top: 0,
+      left: 0,
+      height: 0,
+      width: 0
+    };
+    this.wordRect = {
       top: 0,
       left: 0,
       height: 0,
       width: 0
     };
   }
+  static stylesheetUrl = "highlight-box.css";
   static properties = {
-    rect: {},
+    lineRect: {},
+    wordRect: {},
   }
   static get queries() {
     return {
       highlightElem: ".highlight",
+      wordAreaElem: ".word-highlight",
       topElem: ".side.top",
       leftElem: ".side.left",
       rightElem: ".side.right",
       bottomElem: ".side.bottom",
     }
   }
-  createRenderRoot() {
-    return this;
-  }
   render() {
-    console.log("render()");
-    let { top, left, height, width } = this.rect;
+    let { top, left, height, width } = this.lineRect;
     let bottom = top+height;
     let right = left+width;
+    console.log(`render, top: ${top}, left: ${left}, width: ${width}, height: ${height}, bottom: ${bottom}, right: ${right}`);
     return html`
-        <div class="highlight"   style=${styleMap({ top: `${top}px`, left: `${left}px`, height: `${height}px`, width: `${width}px` })}></div>
-        <div class="side top"    style=${styleMap({ top: 0, left: 0, height: `${top}px`, width: '100%' } )}></div>
-        <div class="side left"   style=${styleMap({ top: `${top}px`, left: 0, height: `${height}px`, width: `${left}px` })}></div>
-        <div class="side right"  style=${styleMap({ top: `${top}px`, height: `${height}px`, left: `${right}px`, width: `calc(100% - ${right}px)` })}></div>
-        <div class="side bottom" style=${styleMap({ top: `${bottom}px`, left:0, height:`calc(100% - ${bottom}px)`, width: '100%' })}></div>
+        <link rel="stylesheet" href=${this.constructor.stylesheetUrl} />
+        <div class="highlight" style=${styleMap({
+          top: `${top}px`,
+          left: `${left}px`,
+          height: `${height}px`,
+          width: `${width}px`
+        })}>
+          <div class="word-highlight"  style=${styleMap({
+            left: `${this.wordRect.x}px`,
+            transform: `translate(${this.wordRect.x}px, 0px)`,
+            height: `${this.wordRect.height}px`,
+            width: `${this.wordRect.width}px`
+          })}></div>
+        </div>
+        <div class="side top"    style=${styleMap({
+          top: 0, left: 0, height: `${top}px`, width: '100%' }
+        )}></div>
+        <div class="side left"   style=${styleMap({
+          top: `${top}px`, left: 0, height: `${height}px`, width: `${left}px`
+        })}></div>
+        <div class="side right"  style=${styleMap({
+          top: `${top}px`, height: `${height}px`, left: `${right}px`, width: `calc(100% - ${right}px)`
+        })}></div>
+        <div class="side bottom" style=${styleMap({
+          top: `${bottom}px`, left:0, height:`calc(100% - ${bottom}px)`, width: '100%'
+        })}></div>
     `;
   }
-  connectedCallback() {
-    super.connectedCallback();
-    console.log("connectedCallback()");
-  }
-  willUpdate() {
-    console.log("willUpdate on HighlightBox");
-  }
-  positionChild(name, dims) {
-    let elem = this[name];
-    if (!elem) {
-      console.log(elem, name +" lookup failed");
-      return;
+  renderAreas(lineRect = null, wordRect = null) {
+    if (lineRect) {
+      this.lineRect = lineRect
     }
-
-    for (let [prop, value] of Object.entries(dims)) {
-      elem.style[prop] = typeof value == "string" ? value : `${value}px`;
+    if (wordRect) {
+      this.wordRect = wordRect;
     }
-  }
-  moveTo(targetRect = {}) {
-    Object.assign(this.rect, targetRect);
-    this.requestUpdate();
-  }
-  moveToElement(targetElem, offsets = [0,0]) {
-    console.log("moveToElement:", targetElem, offsets);
-    const coords = targetElem.getBoundingClientRect();
-    const [xOffset, yOffset] = offsets;
-    const { scrollTop, scrollLeft } = targetElem.ownerDocument.documentElement;
-    const lineHeight = parseFloat(getComputedStyle(targetElem).lineHeight);
-    const padding = {
-      left: 10,
-      right: 10,
-      top: 5,
-      bottom: 5,
-    };
-    const rect = {
-      left: coords.left + scrollLeft - padding.left,
-      top: coords.top + scrollTop - padding.top + yOffset,
-      width: coords.width + padding.left + padding.right,
-      height: lineHeight + padding.top + padding.bottom,
-    };
-    console.log("Moving highlight to rect:", rect);
-    this.moveTo(rect);
+    console.log("renderAreas, ", lineRect, wordRect);
   }
 }
 customElements.define("highlight-box", HighlightBox);
